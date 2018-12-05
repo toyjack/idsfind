9826d68781303bb7515650a82af844aa58ff96fa
toyjack <toyjack@gmail.com>
2018-12-05 18:58:57 +0900

    big big bug fixed

---
+++ data/idsdata/index.js
@@ -0,0 +1,2 @@
+var decomposes=[require("./decompose0"),require("./decompose1"),require("./decompose2"),require("./decompose3"),require("./decompose4"),require("./decompose5"),require("./decompose6")]
+module.exports={decomposes:decomposes};
